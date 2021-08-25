aef1d9c9a5391b312b9d8ffb06a1e12c977c2e62
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-08-25 14:24:21 +0100

    delete auth app. set up postgresql

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tasks",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -76,8 +77,11 @@ WSGI_APPLICATION = "ToDo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ToDo_DB",
+        "USER": "postgres",
+        "PASSWORD": "impeccable2019",
+        "HOST": "localhost",
     }
 }
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class TodoList(models.Model):
+    title = models.CharField(max_length=18)
