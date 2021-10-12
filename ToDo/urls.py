23fb93628853fee7e874e9c394da73434461d696
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-10-12 22:00:09 +0100

    continue authentication implementation

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls")),
-    path("register/", include("users.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
