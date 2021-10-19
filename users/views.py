6bd5d7e7940aa8c8cba86952af5184954822af43
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-10-19 23:09:30 +0100

    updates to deploy on heroku

---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render, HttpResponseRedirect, reverse
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
+
+# Create your views here.
+def register(request):
+  if request.method == 'POST':
+    form = CreateUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      return redirect('login')
+
+  else:
+    form = CreateUserForm()
+  return render(request, 'users/register.html', {'form': form})
