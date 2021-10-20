1d2d7bb00916e149d1123f104c468fdda091c60b
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-10-21 00:57:26 +0100

    update settings

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 
 # from decouple import config
@@ -87,8 +86,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
