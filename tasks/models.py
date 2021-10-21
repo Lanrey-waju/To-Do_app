bb6abfb14ee5eca0fdb4d565178b56b071add749
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-10-21 22:43:49 +0100

    add fields to todolist models

--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.base import Model
 from django.forms.widgets import Widget
 
 # Create your models here.
 class TodoList(models.Model):
+    productiver = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     task_title = models.CharField(max_length=50, null=True)
     task_description = models.TextField(max_length=200, null=True)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.task_title
+
+    class Meta:
+        ordering = ['completed']
