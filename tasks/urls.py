39e1874875e3d98dc1dc52c5575ca474df57be77
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-02 21:21:17 +0100

    fix tasks update view and create update-task html

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 # create routes here
 urlpatterns = [
     path('', views.add_task, name='add-task'),
-    path('delete-task/<int:todos_id>', views.delete_task, name='delete-task')
+    path('delete-task/<int:todos_id>', views.delete_task, name='delete-task'),
+    path('update-task/<int:todos_id>', views.update_task, name='update-task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -19,7 +19,6 @@ def add_task(request):
 
     else:
         form = NewTaskForm(request.POST)
-
         if form.is_valid():
             # Bind the logged in user as the 'productiver'
             new_form = form.save(commit=False)
@@ -33,15 +32,24 @@ def add_task(request):
 
 
 # View to handle update of tasks
-def update_tasks(request, todos_id):
-    # todos = TodoList.objects.get(id=todos_id)
-    # if request.method =="POST":
-    #     pass
-    pass
+@login_required
+def update_task(request, todos_id):
+    productiver = request.user
+    todo = TodoList.objects.get(id=todos_id)
+    if request.method != "POST":
+        form = NewTaskForm(instance=todo)
+    else:
+        form = NewTaskForm(instance=todo, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add-task')    
 
+    context = {"todo": todo, "form": form, "productiver": productiver}
+    return render(request, "tasks/update_task.html", context)
 
 
 # View function to handle tasks deletion
+@login_required
 def delete_task(request, todos_id):
     todos = TodoList.objects.get(id=todos_id)
     if request.method == 'POST':
