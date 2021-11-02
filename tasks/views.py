98b7eb3f859e29990d4c1c2cc1611f8449fa2326
Abdulmumin Akinde <lanreywaju97@gmail.com>
2021-11-02 23:22:43 +0100

    protect and restrict user's tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,9 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404
 from .forms import NewTaskForm
 from .models import TodoList
 # from .models import TodoList
@@ -12,6 +12,7 @@ from .models import TodoList
 # Create your views here.
 @login_required
 def add_task(request):
+    # tasks = TodoList.objects.all()
     productiver = request.user
     todos = TodoList.objects.filter(productiver=productiver)
     if request.method != "POST":
@@ -26,7 +27,8 @@ def add_task(request):
             new_form.save()
             messages.success(request, "Task added successfully.")
             return HttpResponseRedirect(reverse("add-task"))
-
+    # if tasks.productiver != request.user:
+        raise Http404
     context = {"todos": todos, "form": form, "productiver": productiver}
     return render(request, "tasks/add_task.html", context)
 
@@ -36,6 +38,8 @@ def add_task(request):
 def update_task(request, todos_id):
     productiver = request.user
     todo = TodoList.objects.get(id=todos_id)
+    if todo.productiver != request.user:
+        raise Http404
     if request.method != "POST":
         form = NewTaskForm(instance=todo)
     else:
